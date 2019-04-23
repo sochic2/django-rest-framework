e8b094f2a7f96b2508ce9d5dbca514dbe9a6fcb1
sochic2 <netzzang12@gmail.com>
2019-04-23 06:35:49 +0000

    01_Artist_list & Artist_detail & Comment_create

--- musics/views.py
+++ musics/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Music
-from .serializers import MusicSerializer
+from .models import Music, Artist, Comment
+from .serializers import MusicSerializer, ArtistSerializer, ArtistDetailSerializer, CommentSerializer
 
 # Create your views here.
 
@@ -15,11 +15,36 @@ from .serializers import MusicSerializer
 # 그리고 응답하는 함수는 Response 이다.
 # 결과로 보내줄 데이터는 .data로 가져온다.
 
-
 @api_view(['GET'])
 def music_list(request):
     musics = Music.objects.all()    
     serializer = MusicSerializer(musics, many=True)
     return Response(serializer.data)
     
-        
+    
+@api_view(['GET'])
+def music_detail(request, music_pk):
+    music = get_object_or_404(Music, pk=music_pk)
+    serializer = MusicSerializer(music)         #하나만 넘어올땐 many=True 안씀
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def artist_list(request):
+    artists = Artist.objects.all()
+    serializer = ArtistSerializer(artists, many=True)
+    return Response(serializer.data)
+    
+@api_view(['GET'])    
+def artist_detail(request, artist_pk):
+    artist = get_object_or_404(Artist, pk=artist_pk)
+    serializer = ArtistDetailSerializer(artist)
+    return Response(serializer.data)
+    
+@api_view(['POST'])
+def comment_create(request, music_pk):
+    serializer = CommentSerializer(data = request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(music_id=music_pk)
+        return Response(serializer.data)
+    
+    
